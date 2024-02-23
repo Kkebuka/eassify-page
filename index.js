3e35ca4b662999492b59dee211f983ae37d850ca
kkebuka <kkebuka@gmail.com>
2024-02-23 20:48:38 +0100

    other files

---
+++ index.js
@@ -0,0 +1,40 @@
+
+const time = document.querySelectorAll('.time');
+
+function calculateTimeLeft(){
+
+
+    let futureDate = new Date(2024, 3, 24);
+
+    futureDate = futureDate.getTime();
+
+    const todayDate = new Date().getTime();
+    const t = futureDate - todayDate;
+
+    const month = Math.floor(t/(30*24*60*60*1000));
+    const day = Math.floor(t % (30*24*60*60*1000) / (24*60*60*1000));
+    const hour = Math.floor((t % (24*60*60*1000)) / (60*60*1000));
+    const minutes = Math.floor ((t % (60*60*1000) / (60*1000)));
+    const seconds = Math.floor ((t%(60*1000) / 1000));
+    console.log(time)
+
+    const values = [month, day, hour, minutes, seconds];
+
+    time.forEach(function(item, index){
+        let digit = item.innerHTML = values[index];
+        console.log(digit)
+        if(digit < 10){
+            item.innerHTML = `0 ${values[index]}`
+        }
+    })
+
+    startCountdown();
+
+}
+
+function startCountdown(){
+    setInterval(calculateTimeLeft, 1000)
+}
+
+
+calculateTimeLeft()
