5fbd9ba5bfd94ebd2a40567374826a6e45f4b2d8
kkebuka <kkebuka@gmail.com>
2024-07-18 22:55:40 +0100

    time change

--- index.js
+++ index.js
@@ -1,70 +1,57 @@
+const time = document.querySelectorAll(".time");
 
-const time = document.querySelectorAll('.time');
+function calculateTimeLeft() {
+  let futureDate = new Date(2024, 9, 24);
 
-function calculateTimeLeft(){
+  futureDate = futureDate.getTime();
 
+  const todayDate = new Date().getTime();
+  const t = futureDate - todayDate;
 
-    let futureDate = new Date(2024, 3, 24);
+  const month = Math.floor(t / (30 * 24 * 60 * 60 * 1000));
+  const day = Math.floor(
+    (t % (30 * 24 * 60 * 60 * 1000)) / (24 * 60 * 60 * 1000)
+  );
+  const hour = Math.floor((t % (24 * 60 * 60 * 1000)) / (60 * 60 * 1000));
+  const minutes = Math.floor((t % (60 * 60 * 1000)) / (60 * 1000));
+  const seconds = Math.floor((t % (60 * 1000)) / 1000);
 
-    futureDate = futureDate.getTime();
+  const values = [month, day, hour, minutes, seconds];
 
-    const todayDate = new Date().getTime();
-    const t = futureDate - todayDate;
+  time.forEach(function (item, index) {
+    let digit = (item.innerHTML = values[index]);
 
-    const month = Math.floor(t/(30*24*60*60*1000));
-    const day = Math.floor(t % (30*24*60*60*1000) / (24*60*60*1000));
-    const hour = Math.floor((t % (24*60*60*1000)) / (60*60*1000));
-    const minutes = Math.floor ((t % (60*60*1000) / (60*1000)));
-    const seconds = Math.floor ((t%(60*1000) / 1000));
+    if (digit < 10) {
+      item.innerHTML = `0 ${values[index]}`;
+    }
+  });
 
-
-    const values = [month, day, hour, minutes, seconds];
-
-    time.forEach(function(item, index){
-        let digit = item.innerHTML = values[index];
-        
-        if(digit < 10){
-            item.innerHTML = `0 ${values[index]}`
-        }
-    })
-
-    startCountdown();
-
+  startCountdown();
 }
 
-function startCountdown(){
-    setInterval(calculateTimeLeft, 1000)
+function startCountdown() {
+  setInterval(calculateTimeLeft, 1000);
 }
 
+calculateTimeLeft();
+document.addEventListener("DOMContentLoaded", animationEffect);
 
-calculateTimeLeft()
-document.addEventListener("DOMContentLoaded", animationEffect)
+function animationEffect() {
+  const times = document.querySelectorAll(".count-div");
 
-
-function animationEffect(){
-    
-    const times = document.querySelectorAll('.count-div');
-    
-    
-
-
-    times.forEach((time, index) => {
-
-
-        time.style.scale = '1'
-        time.style.opacity = '1'
-        setTimeout(() => {
-            time.style.transform = 'translateY(0)';
-        }, index * 100);
-    });
-
-};
-
-const stayText = document.querySelector('#stays');
-
-function dropElement(){
-setTimeout(stayText.style.transform = 'translateY(0)', 1000);
+  times.forEach((time, index) => {
+    time.style.scale = "1";
+    time.style.opacity = "1";
+    setTimeout(() => {
+      time.style.transform = "translateY(0)";
+    }, index * 100);
+  });
 }
 
-window.onload = dropElement
+const stayText = document.querySelector("#stays");
 
+function dropElement() {
+  setTimeout((stayText.style.transform = "translateY(0)"), 1000);
+}
+
+window.onload = dropElement;
